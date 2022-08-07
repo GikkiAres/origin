4b46947f91ee47a959d79193d80810d2df6bde36
GikkiAres <GikkiAres@icloud.com>
2022-08-07 19:14:31 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // 网站的一些基本配置
+    // base:配置部署站点的基础路径，后续再介绍
+    title: '穆瑾轩', // 网站的标题
+    description: '穆瑾轩测试网页', // 网站的描述，它将会以 <meta> 标签渲染到当前页面的 HTML 中。
+    head: [
+        ['link', { rel: 'icon', href: '/logo.png' }] // 需要被注入到当前页面的 HTML <head> 中的标签
+    ],
+    lang: 'zh-CN',
+    title: '你好， VuePress ！',
+    description: '这是我的第一个 VuePress 站点',
+}
